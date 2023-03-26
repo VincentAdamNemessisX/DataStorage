a751892369bb7068be82783332deff314123d770
VincentAdamNemessis <vincentadamnemessis@gmail.com>
2023-03-26 17:09:53 +0800

    Collection zhihu discovery page's questions, and storage to local txt files, rebase the directories

--- zhihuDiscoveryQuestions/zhihuDiscovery.py
+++ zhihuDiscoveryQuestions/zhihuDiscovery.py
@@ -1,18 +1,17 @@
 """
-# File       : fileStorage.py
+# File       : zhihuDiscovery.py
 # Time       : 3:15 PM
 # Author     : vincent
 # version    : python 3.8
 # Description:
 """
-import os.path
-
 # file storage
 # txt storage
 import requests
 from pyquery import PyQuery
 
 
+# save question to the dict of quests
 def save_titles(contents):
     global index
     for t in contents:
@@ -20,6 +19,7 @@ def save_titles(contents):
         index += 1
 
 
+# write all questions in quests to file
 def write_to_file():
     global quests
     with open('hotQuestions.txt', 'w+') as f:
@@ -31,19 +31,18 @@ url = 'https://www.zhihu.com/explore'
 headers = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:109.0) Gecko/20100101 Firefox/111.0',
 }
-html = requests.get(url, headers=headers).text
-doc = PyQuery(html)
-quests = {}
-# with open('test.html', 'w+', encoding='utf-8') as f:
-# f.write(html)
 index = 0
+# saved class name that used by questions(Under analysis the pages content)
 titles_class = ['ExploreRoundtableCard-title', 'ExploreRoundtableCard-questionTitle',
                 'ExploreSpecialCard-contentTitle', 'css-1nd7dqm',
                 'ExploreCollectionCard-contentTitle']
+quests = {}
+html = requests.get(url, headers=headers).text
+doc = PyQuery(html)
+# with open('test.html', 'w+', encoding='utf-8') as f:
+# f.write(html)
 for i in range(len(titles_class)):
     titles = doc("." + titles_class[i]).items()
     save_titles(titles)
 write_to_file()
-# question = temp[0].strip()
-# author = item[1].lstrip('一').strip()
-# print(question, ":", author)
+
