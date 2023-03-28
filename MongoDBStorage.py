59c6696874981275bb7bb4fbe51598c2cae5e291
VincentAdamNemessis <vincentadamnemessis@gmail.com>
2023-03-28 18:24:20 +0800

    Collection zhihu discovery page's questions, and storage to local txt files, using json storage files and read, using csv storage files and read, using mysql database storage and read data, using mongodb database storage and read data...... rebase the directories

--- MongoDBStorage.py
+++ MongoDBStorage.py
@@ -6,6 +6,8 @@
 # Description:
 """
 import pymongo
+from bson import ObjectId
+
 client = pymongo.MongoClient("mongodb+srv://"
                              "vincent:ZTXic3344"
                              "@tempcluster.kslgvab.mongodb.net/"
@@ -48,6 +50,55 @@ student3 = {
     'gender': 'Male'
 }
 # result = collection.insert_many([student1, student2, student3])
-# print(result)
+# print(result.inserted_ids)
 
+# query data
+# result = collection.find_one({'name': 'Jordan'})
+# print(type(result))
+# print(result)
+# result = collection.find_one({'_id': ObjectId('6422a9e61bec7b1f2abe36db')})
+# print(result)
+# result = collection.find({'age': '30'})
+# print(type(result))
+# for item in result:
+#     print(item)
+# result = collection.find({'age': {'$gt': '10'}})
+# print(type(result))
+# for item in result:
+#     print(item)
+
+# count number
+# 构建聚合查询管道
+pipeline = [
+    {'$group': {'_id': None, 'count': {'$sum': 1}}}
+]
+# 执行聚合查询，并获取文档数量
+# result = collection.aggregate(pipeline)
+# count = result.next()['count']
+# print(count)
+
+# sort data
+# results = collection.find().sort('name', pymongo.ASCENDING)
+# print([rst['name'] for rst in results])  # 列表推导式
+
+# offset
+# results = collection.find().sort('name', pymongo.DESCENDING).skip(1)
+# print([result['name'] for result in results])
+# results = collection.find().sort('name', pymongo.DESCENDING).skip(1).limit(1)
+# print([result['name'] for result in results])
+# results = collection.find({'_id': {'$gt': ObjectId('6422a9e61bec7b1f2abe36db')}})
+# print([(result['name'], result['age']) for result in results])
+
+# update data
+condition = {'name': 'Jordan'}
+stu = collection.find_one(condition)
+stu['age'] = 99
+result = collection.update_one(condition, {'$set': stu})
+print(result)
+print(result.matched_count, result.modified_count)
+
+condition = {'age': {'$gt': 15}}
+result = collection.update_many(condition, {'$inc': {'age': 5}})
+print(result)
+print(result.matched_count, result.modified_count)
 
