13e2c4867a28348f3c0bd2609ca6d412dfb8b20d
VincentAdamNemessis <vincentadamnemessis@gmail.com>
2023-03-29 17:39:16 +0800

    Collection zhihu discovery page's questions, and storage to local txt files, using json storage files and read, using csv storage files and read, using mysql database storage and read data, using mongodb database storage and read data, using redis database storage and read data, The End. rebase the directories

--- redisDB.py
+++ redisDB.py
@@ -5,3 +5,25 @@
 # version    : python 3.8
 # Description:
 """
+from redis import StrictRedis, ConnectionPool, Redis
+
+# redis connect
+# redis = StrictRedis(host='8.130.73.157', port=6379, db=0, password='ic34')
+# redis.set('name', 'Bob')
+# print(redis.get('name'))
+# redis.close()
+
+# redis connection pool
+url = 'redis://:ic34@8.130.73.157:6379/0'  # redis: //:password@host:port/db_number
+# url = 'redis://[:ic34]@8.130.73.157:6379/0'  # redis: //:redis: //:[password]@host:port/db_number
+# url = 'unix://[:ic34]@8.130.73.157:6379/to/socket.sock?db=0' # redis: //:redis:  //:[password]@/path/to/socket.sock?db=db
+pool = ConnectionPool.from_url(url)
+# pool = ConnectionPool(host='8.130.73.157', port=6379, db=0, password='ic34')
+redis = Redis(connection_pool=pool)
+redis.set('hello', 'world!')
+print(redis.get('hello'))
+
+
+
+
+redis.close()
